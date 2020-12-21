54d75474e691a1a8a46dee459e781e1f2126f484
alex held <git@alexheld.io>
2020-12-22 00:45:21 +0100

    refactor: extract toConsoleString into fun

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,9 +3,12 @@ import model.ClientBuilder
 
 fun main(args: Array<String>) {
 	val client = createClientInternal( "Alexander", "Held" ,"0_alexheld","Cologne", "MegaCorp")
-	println("Created client is: $client")
+	println("Created client is: " + client.toConsoleString() )
 }
 
+fun Client.toConsoleString(): String {
+	return "${twitter.handle} ${company.name}"
+}
 
 
 fun createClientInternal(fn: String, ln: String, twitter: String, city: String, company: String): Client {
