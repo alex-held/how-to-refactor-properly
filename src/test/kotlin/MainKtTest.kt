a7fc0f2050670b67db2268329a4dc903b9eb7d6f
alex held <git@alexheld.io>
2020-12-22 01:01:13 +0100

    test: test consoleString

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -1,13 +1,23 @@
 import model.*
-import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Test
 import kotlin.test.*
 
 internal class MainKtTest {
 
+	private fun createSubject(
+		fn: String? = null,
+		ln: String? = null,
+		twitter: String? = null,
+		city: String? = null,
+		company: String? = null,
+	): Client = Client(fn ?: "Alexander", ln ?: "Held",
+		Company(company ?: "MegaCorp", city ?: "Cologne"),
+		Twitter(twitter ?: "0_alexheld"))
+
+
 	@Test
-	fun createClientInternal() {
-		val expected = Client("Alexander", "Held", Company("MegaCorp", "Cologne"), Twitter("0_alexheld"))
+	fun createClientInternal_should_return_client_with_same_values() {
+		val expected = createSubject()
 
 		val result = createClientInternal(
 			expected.firstName,
@@ -22,5 +32,15 @@ internal class MainKtTest {
 		assertEquals(expected.twitter.handle, result.twitter.handle)
 		assertEquals(expected.company.name, result.company.name)
 		assertEquals(expected.company.city, result.company.city)
+	}
+
+	@Test
+	fun consoleString_should_return_correct_description(){
+		val expected = "0_alexheld MegaCorp"
+		val subject = createSubject()
+
+		val actual = subject.consoleString
+
+		assertEquals(expected, actual)
 	}
 }
